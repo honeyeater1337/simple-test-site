b5cebb230671f74e560887aaf2bd92c28e98c88b
honeyeater1337 <113928323+honeyeater1337@users.noreply.github.com>
2022-09-28 15:43:41 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,12 +3,18 @@ var y=0;
 var ans = document.getElementById("answer");
 answer.onclick = function() 
 {
+if (y < 10) {
 x = document.getElementById("forUser").value;
-console.log(x);
 x = x.replace(/[a-z]/g, '');
 x = x.replace(/[^qruopsdgjcb]/gi, '');
 console.log(x);
 document.getElementById("output").innerHTML = x;
 document.getElementById("forUser").value = "";
 document.getElementById("output2").innerHTML = ++y;
+} 
+else { 
+document.getElementById("output").innerHTML = "You have reached the maximum amount";
+document.getElementById("forUser").value = "";
+document.getElementById("output2").innerHTML = y;
+}
 };
